d6ff07888b6733a67603e81fa89f4ceba8b77111
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-10-11 16:02:35 +0530

    added structure

--- stats.h
+++ stats.h
@@ -1,4 +1,10 @@
-
+struct Stats
+{
+    
+    float average;
+    float min;
+    float max;
+};
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
