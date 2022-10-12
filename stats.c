69a8270afb1fb867fcac72cda130396ca599cc44
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-10-12 14:58:07 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,12 +1,50 @@
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    struct Stats s {0,0,0};
+     
+    int i ;
+    if (setlength > 0)
+    {
+       s.min = numberset[0];
+       for (i = 0; i < setlength; i++)
+	   {
+			s.average += numberset[i];
+
+			if (numberset[i] > s.max)
+			{
+				s.max = numberset[i];
+			}
+
+			if (numberset[i] < s.min)
+			{
+				s.min = numberset[i];
+			}
+		}
+
+		 s.average /= setlength;
+        
+     }
+    
+    else
+    {
+        s.average = 0;
+		s.min = 0;
+		s.max = 0;
+    
+    }
+    
    
     return s;
+}
+
+void check_and_alert(float maxThreshold, alerter_funcptr alert_array[],Stats computedStats)
+{
+	if (computedStats.max > maxThreshold)
+	{
+		alert_array[0]();
+		alert_array[1]();
+	}
 }
 
 
