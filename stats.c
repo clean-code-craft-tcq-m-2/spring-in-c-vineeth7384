88a6f1220e838adda2f45e653da20c4624c3fd46
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-10-12 09:47:28 +0530

    removed

--- stats.c
+++ stats.c
@@ -10,8 +10,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.numberset = numberset;
     return s;
 }
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
-{};
+
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
