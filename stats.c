981a4723b78907ff0449139420cacd377dda256c
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-10-12 09:33:32 +0530

    updated Stats compute_statistics function

--- stats.c
+++ stats.c
@@ -6,8 +6,8 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
-    setlength = 0;
-    numberset = NULL;
+    s.setlength = setlength;
+    s.numberset = numberset;
     return s;
 }
 
